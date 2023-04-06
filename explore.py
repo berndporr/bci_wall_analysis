fce96c1eaecacf857b3a59ce119164695ebe3405
Bernd Porr <mail@berndporr.me.uk>
2023-04-06 10:44:44 +0200

    Explore now explores all tasks

--- explore.py
+++ explore.py
@@ -6,48 +6,55 @@ import getopt
 from scipy import signal
 import researchdata1258
 
-# check if we run this as a main program
-if __name__ == "__main__":
-    subj = 1
-    startsec = 60
-    endsec = False
-    task = researchdata1258.Tasks.TASKS[0]
+subj = 1
+startsec = 60
+endsec = False
+task = False
 
-    helptext = 'usage: {} -p participant -s startsec -e endsec -f noiseredfile.tsv -t task -m -h'.format(sys.argv[0])
-    helptext = helptext + "\nOption -m switches over to matplotlib. Default is plotly."
+helptext = 'usage: {} -p participant -s startsec -e endsec -f noiseredfile.tsv -t task -m -h'.format(sys.argv[0])
+helptext = helptext + "\nOption -m switches over to matplotlib. Default is plotly."
 
-    try:
-        # Gather the arguments
-        all_args = sys.argv[1:]
-        opts, arg = getopt.getopt(all_args, 'p:s:e:f:t:mh')
-        # Iterate over the options and values
-        for opt, arg_val in opts:
-            if '-p' in opt:
-                subj = int(arg_val)
-            elif '-s' in opt:
-                startsec = int(arg_val)
-            elif '-e' in opt:
-                endsec = int(arg_val)
-            elif '-t' in opt:
-                task = arg_val
-            elif '-h' in opt:
-                raise getopt.GetoptError(helptext)
-            else:
-                raise getopt.GetoptError(helptext)
-    except getopt.GetoptError as err:
-        print (err)
-        sys.exit(2)
+try:
+    # Gather the arguments
+    all_args = sys.argv[1:]
+    opts, arg = getopt.getopt(all_args, 'p:s:e:f:t:mh')
+    # Iterate over the options and values
+    for opt, arg_val in opts:
+        if '-p' in opt:
+            subj = int(arg_val)
+        elif '-s' in opt:
+            startsec = int(arg_val)
+        elif '-e' in opt:
+            endsec = int(arg_val)
+        elif '-t' in opt:
+            task = arg_val
+        elif '-h' in opt:
+            raise getopt.GetoptError(helptext)
+        else:
+            raise getopt.GetoptError(helptext)
+except getopt.GetoptError as err:
+    print (err)
+    sys.exit(2)
 
-    data = researchdata1258.Tasks(subj,task,startsec,endsec)
+fig, axs = plt.subplots(len(researchdata1258.Tasks.TASKS), 2,sharex='col')
 
-    plt.figure("Timedomain: {} {}".format(subj,task))
-    plt.xlabel("time/sec")
-    plt.ylabel("EEG/uV")
-    plt.plot(data.t,data.ch1 * 1E6)
-    plt.figure("Spectrum: {} {}".format(subj,task))
-    plt.xlabel("Frequency/Hz")
-    plt.ylabel("Power/V^2")
-    fx, fy = signal.periodogram(data.ch1,data.Fs,scaling="spectrum",nfft=data.Fs)
-    plt.xlim([0,100])
-    plt.plot(fx,fy)
-    plt.show()
+def plotTask(r,tn):
+    data = researchdata1258.Tasks(subj,tn,startsec,endsec)
+    fx, fy = signal.periodogram(data.ch1,data.Fs,scaling="spectrum",nfft=(data.Fs*10))
+    axs[r,0].set_xlabel("time/sec")
+    axs[r,0].set_ylabel("{}\nEEG/uV".format(tn))
+    axs[r,0].set_ylim([-300,300])
+    axs[r,0].set_xlim([0,60])
+    axs[r,0].plot(data.t,data.ch1 * 1E6)
+    axs[r,1].set_xlabel("F/Hz")
+    axs[r,1].set_ylabel("P/1E10 V^2")
+    axs[r,1].set_xlim([0,100])
+    axs[r,1].set_ylim([1E-14,1E-10])
+    axs[r,1].semilogy(fx,fy)
+    
+
+for t in range(len(researchdata1258.Tasks.TASKS)):
+    tn = researchdata1258.Tasks.TASKS[t]
+    plotTask(t,tn)
+    
+plt.show()
